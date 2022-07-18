d63950a9dff687f93774fef3b3b5ded2582a41e1
Sejan <prantosm@gmail.com>
2022-07-18 06:01:37 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from './components/Navbar/Navbar';
+import About from './containers/About/About';
+import Footer from './containers/footer/Footer';
+import Header from './containers/Header/Header';
+import Skills from './containers/Skills/Skills';
+import Testimonial from './containers/testimonial/Testimonial';
+import Work from './containers/Work/Work';
+import "./App.css";
+//import About from './containers/About/About';
+//import About from './containers/About/About';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Navbar/>
+      <Header/>
+      <About/>
+      <Work/>
+      <Skills/>
+      <Testimonial/>
+      <Footer/>
     </div>
   );
 }
